dda6183b706d676d1942706364986fcb9ecdebd2
alesto-krk <aleksandrajaworska87@gmail.com>
2021-03-05 15:39:29 +0100

    telebook podzielony na metody

---
+++ src/sample/Main.java
@@ -0,0 +1,148 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main extends Application {
+
+    List<Telebook> listaNumerow = new LinkedList<>();
+    Stage primaryStage;
+    Label emptyLabel = new Label("");
+
+    public GridPane dodajDoListy(GridPane siatka){
+        //TYTUŁ
+        Image zdjecieTelefonu = new Image(getClass().getResourceAsStream("telefon.jpg"),250,200,true,false);
+        Label title = new Label("TELEBOOK", new ImageView(zdjecieTelefonu));
+        title.setFont(Font.font(30));
+        title.setTextFill(Color.ORANGERED);
+        GridPane.setConstraints(title,1,1);
+        siatka.getChildren().add(title);
+        //DODAJ
+        Label wpiszImie = new Label("Wpisz imie:");
+        TextField imie = new TextField("wpisz imie");
+        Label wpiszNr = new Label("Wpisz numer telefonu:");
+        TextField telefon = new TextField("wpisz nr telefonu");
+        Button dodaj = new Button("Dodaj");
+        dodaj.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("dodano " + imie.getText() + " " + telefon.getText());
+                listaNumerow.add(new Telebook(imie.getText(), telefon.getText()));
+                imie.clear();
+                telefon.clear();
+               refreshScene();
+            }
+        });
+        GridPane.setConstraints(wpiszImie,1,2);
+        GridPane.setConstraints(imie,1,3);
+        GridPane.setConstraints(wpiszNr,1,4);
+        GridPane.setConstraints(telefon,1,5);
+        GridPane.setConstraints(dodaj,1,6);
+        siatka.getChildren().addAll(wpiszImie,imie,wpiszNr,telefon,dodaj);
+        return siatka;
+    }
+
+    public GridPane biezacaLista(GridPane siatka){
+        Button listaNumerowButton = new Button("Lista numerów");
+        listaNumerowButton.setOnAction(e -> {
+            //refreshScene();
+            System.out.println("uzupelniono listę");
+            int rowIndex = 1;
+            for(Telebook l: listaNumerow) {
+                Label listaLabela = new Label(rowIndex  + ". " + l.getImie() + " " + l.getNumer());
+                siatka.getChildren().add(listaLabela);
+                siatka.setConstraints(listaLabela, 2, rowIndex+2);
+                rowIndex++;
+            }
+
+        });
+        GridPane.setConstraints(listaNumerowButton,2,2);
+        siatka.getChildren().add(listaNumerowButton);
+        return siatka;
+    }
+
+    public GridPane usunZListy(GridPane siatka){
+        Label usun = new Label("Który kontakt chcesz usunąć? ");
+        TextField usunImie = new TextField("podaj imię ");
+        Button usunButton = new Button("Usuń");
+        usunButton.setOnAction(e -> {
+            for(Telebook l: listaNumerow){
+                if (usunImie.getText().equals(l.getImie())){
+                    System.out.println("erased");
+                    //listaNumerow.
+                    listaNumerow.remove(l);    //TU SIE WYKRZACZA JAK SIE WPISZE W USUN PIERWSZE IMIE Z LISTY, JAK NASTEPNE TO OK
+                    }
+            }
+            /*for(Telebook l: listaNumerow){
+                System.out.println(l.getImie());
+            }*/
+            refreshScene();
+        });
+        GridPane.setConstraints(usun,1,8);
+        GridPane.setConstraints(usunImie,1,9);
+        GridPane.setConstraints(usunButton,1,10);
+        siatka.getChildren().addAll(usun,usunImie,usunButton);
+        return siatka;
+    }
+
+    public GridPane zakoncz(GridPane siatka){
+        Image zdjecieTelefonuKoniec = new Image(getClass().getResourceAsStream("telefon.jpg"),50,40,true,false);
+        Button zakoncz = new Button("Zakończ", new ImageView(zdjecieTelefonuKoniec));
+        zakoncz.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
+            }
+        });
+        GridPane.setConstraints(zakoncz,1,100);
+        GridPane.setConstraints(emptyLabel,1,11);
+        siatka.getChildren().addAll(emptyLabel,zakoncz);
+        return siatka;
+    }
+
+    public Scene homeScene(){
+        var siatka = new GridPane();
+        siatka = dodajDoListy(siatka);
+        siatka = biezacaLista(siatka);
+        siatka = usunZListy(siatka);
+        siatka = zakoncz(siatka);
+        return new Scene(siatka, 600,600);    }
+
+        public void refreshScene(){
+        primaryStage.setScene(homeScene());
+        }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //var siatka = new GridPane();
+        //var scene = new Scene(siatka,600,600);
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Telebook");
+        Image icon = new Image(getClass().getResourceAsStream("sluchawka.png"));
+        primaryStage.getIcons().add(icon);
+        primaryStage.setScene(homeScene());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/Telebook.java
@@ -0,0 +1,57 @@
+package sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Telebook {
+    private Map<String, String> kontakty = new HashMap<>();
+    private String imie = "-";
+    private String numer = "--";
+
+    Telebook(String imie, String numer){
+        this.imie = imie;
+        this.numer = numer;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNumer() {
+        return numer;
+    }
+
+    public Map<String, String> getKontakty() {
+        return kontakty;
+    }
+
+    public void addNewContact(String name, String number) {
+        kontakty.put(name, number);
+    }
+
+    public void eraseContact(String name) {
+        if (kontakty.containsKey(name)) {
+            kontakty.remove(name);
+            System.out.println("Teraz masz " + kontakty.size() + " kontaktów");
+        } else System.out.println("Nie ma takiego kontaktu");
+    }
+
+    public void bookSize() {
+        System.out.println(kontakty.size());
+    }
+    public String koko(){
+        return "kokokokoko";
+    }
+
+    public void searchByName(String fragment) {
+        int r = 0;
+        for (Map.Entry<String, String> e : kontakty.entrySet()) {
+            if (e.getKey().toLowerCase().contains(fragment.toLowerCase())) {
+                System.out.println("Found -> " + e.getKey());
+                r++;
+            }
+        }
+        if (r == 0)
+            System.out.println("Nie ma takiego kontaktu");
+    }
+}
